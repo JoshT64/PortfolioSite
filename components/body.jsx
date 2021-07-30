86857b6da0eb8d96327ec0ba251fca806908e7b4
JoshT64 <joshuatoney55@gmail.com>
2021-07-30 18:08:38 -0400

    Change animations

--- components/body.jsx
+++ components/body.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { useSpring, animated } from 'react-spring';
+import { useSpring, animated, useTrail } from 'react-spring';
 import Card from '../projects/Card';
 
 export default function Body() {
   const [isScrolled, setIsScrolled] = useState(false);
   const [isScrolled2, setIsScrolled2] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
   const props = useSpring({
     to: { opacity: 1, x: 0 },
@@ -29,10 +30,11 @@ export default function Body() {
   });
 
   const props3 = useSpring({
-    to: { opacity: 1, x: 0 },
+    to: isScrolled ? { opacity: 1, x: 0 } : { opacity: 0, x: 0 },
     from: { opacity: 0, x: 25 },
     delay: 300,
     config: { mass: 3, tension: 1700, friction: 1000, duration: 250 },
+    reset: false,
     reverse: isScrolled2 ? true : false,
   });
 
@@ -46,6 +48,7 @@ export default function Body() {
 
   useEffect(() => {
     window.onscroll = () => {
+      setToggle(window.pageYOffset > 380 ? true : false);
       setIsScrolled(window.pageYOffset < 300 ? false : true);
       setIsScrolled2(window.pageYOffset > 380 ? false : true);
       console.log(isScrolled2);
@@ -57,65 +60,71 @@ export default function Body() {
     <div>
       <main
         style={props}
-        className="flex flex-col flex-1 px-20 text-center mb-20"
+        className='flex flex-col flex-1 px-20 text-center mb-20'
       >
         <animated.div style={props}>
-          <h1 className="text-5xl font-bold m-2 mb-2 mt-0 ">Joshua Toney.</h1>
+          <h1 className='text-5xl font-bold m-2 mb-2 mt-0 '>Joshua Toney.</h1>
         </animated.div>
         <animated.div style={props2}>
-          <p style={{ fontSize: '1.2rem' }} className="pt-2 text-gray-600">
+          <p style={{ fontSize: '1.2rem' }} className='pt-2 text-gray-600'>
             Front-end Developer, Designer and React Enthusiast.
           </p>
         </animated.div>
 
         <animated.img
           style={props2}
-          src="miijosh.png"
-          height="216"
-          width="216"
-          className="mt-5 mb-5 border border-purple-500 rounded-full bg-purple-200 self-center"
+          src='miijosh.png'
+          height='216'
+          width='216'
+          className='mt-5 mb-5 border border-purple-500 rounded-full bg-purple-200 self-center'
         />
         <animated.div style={props2}>
-          <p style={{ fontSize: '1.2rem' }} className="text-gray-500">
+          <p style={{ fontSize: '1.2rem' }} className='text-gray-500'>
             I love creating things.
           </p>
-          <p className=" max-w-3xl m-auto mt-2 border-t-2 text-black">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-            convallis eros sem, quis sagittis lacus vulputate non. Integer et
-            rutrum orci, vitae volutpat lorem. Vivamus purus felis, dictum vitae
-            vestibulum non, bibendum vel nisl. Duis nec dignissim nulla, sit
-            amet vehicula est. Nulla ac sem lectus. Vestibulum ullamcorper enim
-            vitae massa tincidunt pretium ac scelerisque erat. Cras finibus
-            iaculis sagittis. Aliquam vehicula lacinia diam, ut semper risus
-            volutpat et. Integer vitae dolor consectetur, semper nulla vel,
-            blandit risus. Donec in vestibulum tellus. Aenean metus arcu,
-            vestibulum vel pharetra in, sodales eget nunc. Nunc scelerisque
-            egestas odio, a cursus mi tempor sit amet.
+          <p className=' max-w-3xl m-auto mt-2 border-t-2 text-gray-600 leading- tracking-wide antialiased'>
+            Passionate about learning and improving all the time. Creating
+            simple, yet powerful designs. Integer et rutrum orci, vitae volutpat
+            lorem. Vivamus purus felis, dictum vitae vestibulum non, bibendum
+            vel nisl. Duis nec dignissim nulla, sit amet vehicula est. Nulla ac
+            sem lectus. Vestibulum ullamcorper enim vitae massa tincidunt
+            pretium ac scelerisque erat. Cras finibus iaculis sagittis. Aliquam
+            vehicula lacinia diam, ut semper risus volutpat et. Integer vitae
+            dolor consectetur, semper nulla vel, blandit risus. Donec in
+            vestibulum tellus. Aenean metus arcu, vestibulum vel pharetra in,
+            sodales eget nunc. Nunc scelerisque egestas odio, a cursus mi tempor
+            sit amet.
           </p>
         </animated.div>
       </main>
       {/* ====== MID SECTION ====== */}
-      <div className=" grid-cols-3 grid  divide-x-2 mt-10">
+
+      <div className=' grid-cols-3 grid  divide-x-2 mt-10'>
         {/* ====== PROJECTS ======== */}
-        <div id="projects" className="mt-4 mb-16 col-span-2 mr-10">
+
+        <div id='projects' className='mt-4 mb-16 col-span-2 mr-10'>
           <br></br>
-          <h1 className="text-3xl bg-purple-100 inline-block">Projects</h1>
-          <animated.div>
-            <Card style={props3} />
-          </animated.div>
+          <h1 className='text-3xl bg-purple-100 inline-block'>Projects</h1>
+          {toggle ? (
+            <animated.div>
+              <Card style={props3} />
+            </animated.div>
+          ) : null}
         </div>
+
         {/* ===== SKILLS ===== */}
-        <div className="   col-span-1">
-          <h1 className=" border-t-2 text-3xl bg-purple-100 inline-block border-gray mt-10  ">
+        <div className='col-span-1'>
+          <h1 className='border-t-2 text-3xl bg-purple-100 inline-block border-gray mt-10'>
             Skills
           </h1>
         </div>
       </div>
+
       {/* ========= ABOUT ME ========= */}
-      <div id="about" className="mt-52">
+      <div id='about' className='mt-52'>
         <br></br>
-        <h1 className="mt-52 text-3xl bg-purple-100 inline-block">About Me</h1>
-        <p style={{ width: '25rem' }} className="m-auto mt-4">
+        <h1 className='mt-52 text-3xl bg-purple-100 inline-block'>About Me</h1>
+        <p style={{ width: '25rem' }} className='m-auto mt-4'>
           Aspiring Web Developer, finding new solutions on the daily. Addicted
           to learning new things.
         </p>
--- projects/Card.jsx
+++ projects/Card.jsx
@@ -18,15 +18,15 @@ export default function Card(props) {
     <animated.div
       style={props.style}
       // style={props}
-      className="flex flex-col justify-center items-center rounded m-8 "
+      className='flex flex-col justify-center items-center rounded m-8 '
     >
       {projects.map((project, idx) => {
         return (
           <animated.div key={idx}>
-            <h1 className="m-2  text-3xl">{`${project.name}:`}</h1>
+            <h1 className='m-2  text-3xl'>{`${project.name}:`}</h1>
             <a href={project.link}>
               <img
-                className="m-4 border"
+                className='m-4 border'
                 style={{
                   borderRadius: '1rem',
                   height: '27rem',
@@ -36,7 +36,7 @@ export default function Card(props) {
             </a>
 
             <p
-              className="text-gray-500 m-auto"
+              className='text-gray-500 m-auto'
               style={{ width: '35rem', textAlign: 'center' }}
             >
               {project.description}
